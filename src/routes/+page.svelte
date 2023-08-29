0df0b0ce679873fb1b7b70e1da6320f753038140
Russell Warner <rewarner@russellbits.com>
2023-08-29 09:17:07 -0400

    Feature: root page route tested for font and image imports.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
+	import '../house.css';
+	import Header from '../components/Header.svelte';
 	export let data;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<Header />
+
+<h1>Welcome to Tagger</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <ul>
@@ -17,3 +21,11 @@
 		<li>{label.name}</li>
 	{/each}
 </ul>
+
+<style>
+	h1 {
+		font-family: 'IBM Plex Serif', 'Georgia', serif;
+		font-weight: 400;
+		font-size: 2em;
+	}
+</style>
