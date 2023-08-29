880ca5cbea2b920f5a86da3322f09ee3aa8f79fb
Russell Warner <rewarner@russellbits.com>
2023-08-29 16:54:36 -0400

    Story: New UI components added to story route.

--- src/routes/story/+page.svelte
+++ src/routes/story/+page.svelte
@@ -8,6 +8,37 @@
 
 <h2>Interactive Components</h2>
 
+<h4>Generic Buttons</h4>
 <Button label={'Suggest Tags'} />
+
+<h4>Add Tag Buttons</h4>
 <Button label={'+ | China'} type={'add'} />
+
+<h4>Remove Tag Buttons</h4>
 <Button label={'x | Government'} type={'remove'} />
+
+<h4>Article Inputs</h4>
+<div class="tag-input">
+	<input type="text" />
+	<button class="add">+</button>
+	<Button label={'Suggest Tags'} />
+</div>
+<div>
+	<Button label={'Party'} type={'add'} /><Button label={'Government'} type={'add'} />
+</div>
+<div class="articleTitle">
+	<h1>How China became big business for Twitter</h1>
+</div>
+<div />
+
+<style>
+	.articleTitle {
+		background-color: rgba(96, 50, 200, 1);
+	}
+	.articleTitle h1 {
+		font-family: 'IBM Plex Serif', 'Georgia', serif;
+	}
+	h4 {
+		padding: 2em 0 0.5em 0;
+	}
+</style>
