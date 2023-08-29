9bd60808fe97896f68b70ce8729ffc5df69816af
Russell Warner <rewarner@russellbits.com>
2023-08-29 12:30:56 -0400

    Styles: added route for viewing UI elements.

---
+++ src/routes/story/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../../house.css';
+	import Header from '$lib/components/Header.svelte';
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<Header />
+
+<h2>Interactive Components</h2>
+
+<Button label={'Suggest Tags'} />
+<Button label={'+ | China'} type={'add'} />
+<Button label={'x | Government'} type={'remove'} />
