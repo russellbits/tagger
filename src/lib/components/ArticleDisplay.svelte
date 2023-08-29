56c7e3b76514b2ded18a23e4c0e4cef7c7be3886
Russell Warner <rewarner@russellbits.com>
2023-08-29 17:26:52 -0400

    Feature: article display created based on story route example.

--- src/lib/components/ArticleDisplay.svelte
+++ src/lib/components/ArticleDisplay.svelte
@@ -1,7 +1,41 @@
-<script></script>
+<script>
+	import Button from './Button.svelte';
+	export let data;
+</script>
 
-<div class="article-display">
-	<h2>Current Article</h2>
+<div class="tag-input">
+	<input type="text" />
+	<button class="add">+</button>
+	<Button label={'Suggest Tags'} />
+</div>
+<div>
+	<Button label={'Party'} type={'add'} /><Button label={'Government'} type={'add'} />
 </div>
 
-<style></style>
+<article>
+	<h1>How China became big business for Twitter</h1>
+	<Button label={'x | China'} type={'remove'} /><Button label={'x | Twitter'} type={'remove'} />
+	<p>
+		A Reuters review of publicly available government tenders, budget documents and promoted tweets
+		from 2020 to 2022 shows local government authorities and Chinese Communist Party propaganda
+		offices for cities, provinces and even districts across the country have flocked to Twitter to
+		buy ads.
+	</p>
+</article>
+
+<style>
+	article > h1 {
+		font-family: 'IBM Plex Serif', 'Georgia', serif;
+		font-size: 1.8em;
+		background-color: rgba(96, 50, 200, 1);
+		padding: 0.5em;
+	}
+	article > p {
+		font-family: 'IBM Plex Serif', 'Georgia', serif;
+		font-size: 1em;
+		color: white;
+	}
+	h4 {
+		padding: 2em 0 0.5em 0;
+	}
+</style>
