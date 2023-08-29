6785cb319804072279e156a852e37f8eff2d9808
Russell Warner <rewarner@russellbits.com>
2023-08-29 16:13:09 -0400

    Feature: tag menu layout, styles, data injection from Supabase.

--- src/lib/components/TagsNav.svelte
+++ src/lib/components/TagsNav.svelte
@@ -4,19 +4,17 @@
 	const labelsNumber = data.labels.length;
 </script>
 
-<div>
-	<h2>{labelsNumber} Labels</h2>
-	<!-- <pre>{JSON.stringify(data.labels.length)}</pre> -->
-	<ul>
-		{#each data.labels as label}
-			{#if label.id == highlight}
-				<li class="highlight"><a href="#">{label.name}</a></li>
-			{:else}
-				<li><a href="#">{label.name}</a></li>
-			{/if}
-		{/each}
-	</ul>
-</div>
+<h2>{labelsNumber} Labels</h2>
+<ul>
+	<li class="main-tag">All Articles</li>
+	{#each data.labels as label}
+		{#if label.id == highlight}
+			<li class="highlight"><a href="#">{label.name}</a></li>
+		{:else}
+			<li><a href="#">{label.name}</a></li>
+		{/if}
+	{/each}
+</ul>
 
 <style>
 	ul {
@@ -28,16 +26,19 @@
 		font-family: 'Inter', 'Helvetica', serif;
 		font-weight: 700;
 		font-size: 0.8em;
-		text-transform: uppercase;
+		text-transform: capitalize;
 		line-height: 1em;
 		min-height: calc((0.8 * 3)+ (1.4 * 3));
-		padding: 0.4em;
+		padding: 0.5em 2.3em;
 	}
 	li:hover {
 		background-color: rgba(0, 169, 10, 1);
 	}
 	li.highlight {
 		background-color: rgba(0, 169, 10, 1);
+	}
+	li.main-tag {
+		text-transform: uppercase;
 	}
 	a {
 		color: white;
