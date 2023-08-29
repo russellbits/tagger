f34c6be2f58c3f0d2dea1c2fe4cb9ad51e5e82fb
Russell Warner <rewarner@russellbits.com>
2023-08-28 21:46:49 -0400

    Feautre: root page addition of iterated article titles.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+	export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ul>
+	{#each data.articles as article}
+		<li>{article.title}</li>
+	{/each}
+</ul>
