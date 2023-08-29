b78c692bb484b7544879416608cb7166a7abdc6b
Russell Warner <rewarner@russellbits.com>
2023-08-29 09:13:42 -0400

    Feature: components directory with Header added.

---
+++ src/components/Header.svelte
@@ -0,0 +1,44 @@
+<script>
+	//import logo from '/images/bilby-logo.svg';
+</script>
+
+<header>
+	<div class="logo">
+		<a href="https://bilby.ai">
+			<img src="/images/bilby-logo.svg" width="146" height="46" alt="Bilbly AI" />
+		</a>
+	</div>
+
+	<div class="greeting">Hi, Russell!</div>
+
+	<nav>
+		<button> Sign Out </button>
+	</nav>
+</header>
+
+<style>
+	header {
+		color: white;
+		display: flex;
+		justify-content: space-between;
+		background-color: black;
+	}
+
+	.logo {
+		padding: 1em;
+	}
+
+	.greeting {
+		font-family: 'IBM Plex Serif', 'Georgia', serif;
+		font-size: 2em;
+		display: flex;
+		justify-content: left;
+	}
+
+	nav {
+		font: 'IBM Plex Sans', sans-serif;
+		font-weight: 600;
+		display: flex;
+		justify-content: right;
+	}
+</style>
